b5b19299b0b35da52889545334b58bca6748fa2a
Reynaldo Hermano <rhermano@asi-dev5.com>
2024-04-18 13:37:23 +0800

    commit fix

--- WebAPI/WebAPI/Controllers/WeatherForecastController.cs
+++ WebAPI/WebAPI/Controllers/WeatherForecastController.cs
@@ -18,6 +18,7 @@ namespace WebAPI.Controllers
             _logger = logger;
         }
 
+
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
