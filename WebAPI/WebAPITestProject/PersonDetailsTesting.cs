17defb456ac7631892fe640f5be828d549075657
rhermano-lt <reynaldohermano3@gmail.com>
2024-04-19 08:15:43 +0800

    commit

---
+++ WebAPI/WebAPITestProject/PersonDetailsTesting.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using WebAPI.Controllers;
+
+namespace WebAPI.Tests
+{
+    public class PersonDetailsTesting
+    {
+        [Fact]
+        public void GetPersonDetails_ReturnsOkResult()
+        {
+            // Arrange
+            var controller = new PersonController();
+
+            // Act
+            var result = controller.GetPersonDetails();
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public void GetPersonDetails_ReturnsCorrectData()
+        {
+            // Arrange
+            var controller = new PersonController();
+            var expectedDetails = new PersonController.PersonDetails
+            {
+                Id = It.IsAny<string>(),
+                Name = "Reynaldo",
+                Age = "25"
+            };
+
+            // Act
+            var result = controller.GetPersonDetails() as OkObjectResult;
+            var actualDetails = result?.Value as PersonController.PersonDetails;
+
+            // Assert
+            Assert.NotNull(actualDetails);
+            Assert.Equal(expectedDetails.Name, actualDetails.Name);
+            Assert.Equal(expectedDetails.Age, actualDetails.Age);
+        }
+    }
+}
